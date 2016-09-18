554fd4f94b12c335e9d1d53c7c6b7f8accfe260e
shinya-watanabe <shinya-watanabe@cookpad.com>
2016-09-18 11:29:14 +0900

    ADd unicorn log

--- ruby/config/unicorn/isuda.rb
+++ ruby/config/unicorn/isuda.rb
@@ -1,3 +1,22 @@
+require 'logger'
+
 worker_processes 5
 preload_app true
+
 timeout 120
+
+pid "/tmp/unicorn.pid"
+stderr_path "/tmp/unicorn-isuda-err.log"
+stdout_path "/tmp/unicorn-isuda-out.log"
+logger Logger.new($stderr).tap{|l| l.formatter = Logger::Formatter.new }
+
+before_fork do |server, worker|
+  old_pid_path = "/home/isucon/webapp/ruby/unicorn-isuda.pid.oldbin"
+  if File.exists?(old_pid_path) && server.pid != old_pid_path
+    begin
+      Process.kill("QUIT", File.read(old_pid_path).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
